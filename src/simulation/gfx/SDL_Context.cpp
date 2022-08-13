e7e16efe60ca17644d1a5d13e5c84f266b11ae36
Akshayaap <Akshaykumarparmar51@gmail.com>
2022-08-13 23:23:50 +0530

    Native Scale Support from SDL

--- src/simulation/gfx/SDL_Context.cpp
+++ src/simulation/gfx/SDL_Context.cpp
@@ -13,9 +13,10 @@ void SDL_Context::DrawImage(interface::gfx::Image *image) {
 
 void SDL_Context::DrawImage(interface::gfx::Image *image, const Vec2 &pos) {
     Renderer::DrawImage(image, pos);
-    SDL_Rect rect = {static_cast<int>(this->pos.GetX()), static_cast<int>(this->pos.GetY()),
-                     static_cast<int>(image->GetSize().GetX() * this->fScale.GetX()),
-                     static_cast<int>(image->GetSize().GetY() * this->fScale.GetY())};
+    SDL_Rect rect = {static_cast<int>(pos.GetX() + this->fTranslate.GetX()),
+                     static_cast<int>(pos.GetY() + this->fTranslate.GetY()),
+                     static_cast<int>(image->GetSize().GetX()),
+                     static_cast<int>(image->GetSize().GetY())};
     SDL_RenderCopy(this->renderer, ((SDL_Image *) image)->GetSDL_Texture(), nullptr, &rect);
 }
 
@@ -36,9 +37,10 @@ void SDL_Context::DrawLine(const Vec2 &v1, const Vec2 &v2) {
     Vec2 end = v2 - v1;
     end *= this->fScale;
     end += this->pos;
+
     SDL_RenderDrawLine(this->GetSDL_Renderer(),
-                       pos.GetX(), pos.GetY(),
-                       end.GetX(), end.GetY());
+                       v1.GetX() + this->fTranslate.GetX(), v1.GetY() + this->fTranslate.GetY(),
+                       v2.GetX() + this->fTranslate.GetX(), v2.GetY() + this->fTranslate.GetY());
 }
 
 void SDL_Context::Scale(double s) {
@@ -47,6 +49,7 @@ void SDL_Context::Scale(double s) {
 
 void SDL_Context::Scale(const Vec2 &center, double s) {
     Renderer::Scale(center, s);
+    SDL_RenderSetScale(this->renderer, this->fScale.GetX(), this->fScale.GetY());
 }
 
 void SDL_Context::Translate(const Vec2 &d) {
@@ -79,9 +82,8 @@ void SDL_Context::Present() {
 
 void SDL_Context::DrawRect(int x, int y, int w, int h) {
     Renderer::DrawRect(x, y, w, h);
-    SDL_Rect rect = {static_cast<int>(pos.GetX()),
-                     static_cast<int>(pos.GetY()),
-                     static_cast<int>(w * fScale.GetX()), static_cast<int>(h * fScale.GetY())};
+    SDL_Rect rect = {x + static_cast<int>( this->fTranslate.GetX()), y + static_cast<int>(this->fTranslate.GetY()), w,
+                     h};
     SDL_RenderFillRect(this->renderer, &rect);
 }
 
@@ -92,8 +94,8 @@ void SDL_Context::DrawPoint(int x, int y) {
 
 void SDL_Context::DrawRect(const Vec2 &pos, const Vec2 &size) {
     Renderer::DrawRect(pos, size);
-    SDL_Rect rect = {static_cast<int>(pos.GetX()),
-                     static_cast<int>(pos.GetY()),
-                     static_cast<int>(size.GetX() * fScale.GetX()), static_cast<int>(size.GetY() * fScale.GetY())};
+    SDL_Rect rect = {static_cast<int>(this->pos.GetX()),
+                     static_cast<int>(this->pos.GetY()),
+                     static_cast<int>(size.GetX()), static_cast<int>(size.GetY())};
     SDL_RenderFillRect(this->renderer, &rect);
 }
